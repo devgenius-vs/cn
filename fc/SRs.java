b5e0d7457aba5ffb794acd60f537dd918635ec69
vijaisuriam <137481937+vijaisuriam@users.noreply.github.com>
2023-11-15 23:01:05 +0530

    Added error and routiing algorithms

--- fc/SRs.java
+++ fc/SRs.java
@@ -1,12 +1,15 @@
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketException;
+
 public class SRs {
     static ServerSocket Serversocket;
     static DataInputStream dis;
     static DataOutputStream dos;
+
     public static void main(String[] args) throws SocketException {
         try {
             int a[] = { 30, 40, 50, 60, 70, 80, 90, 100 };
@@ -26,16 +29,13 @@ public class SRs {
             int k = dis.read();
             dos.write(a[k]);
             dos.flush();
-        }
-        catch (IOException e){
+        } catch (IOException e) {
             System.out.println(e);
-        }
-        finally{
-            try{
+        } finally {
+            try {
                 dis.close();
                 dos.close();
-            }
-            catch (IOException e){
+            } catch (IOException e) {
                 e.printStackTrace();
             }
         }
